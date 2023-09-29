b553aff73532abbfecc24d088f21fd0473777027
KevinKobori <kevin.kobori@hotmail.com>
2023-09-29 20:55:00 -0300

    Added fullName validation

--- lib/fluttely_form_validations.dart
+++ lib/fluttely_form_validations.dart
@@ -1,7 +1,38 @@
 library fluttely_form_validations;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class FormValidations {
+  static String _getInitials(String fullName) {
+    List<String> names = fullName.split(' ');
+    String initials = '';
+    int numWords = 1;
+    if (numWords < names.length) {
+      numWords = names.length;
+    } else {
+      return '';
+    }
+    for (var i = 0; i < numWords; i++) {
+      initials += names[i][0];
+    }
+    return initials;
+  }
+
+  static String? fullName(String? val) {
+    final String? fullName = val;
+    if (fullName?.isEmpty == true) return 'this field can not be empty';
+    if (fullName == null) return 'this field can not be empty';
+
+    var initials = _getInitials(fullName);
+    String patttern = r'(^[a-zA-Z ]*$)';
+    RegExp regExp = RegExp(patttern);
+
+    if (fullName.isEmpty || (fullName.length < 3)) {
+      return 'Enter Your Full Name';
+    } else if (!regExp.hasMatch(fullName)) {
+      return 'Enter Characters From A To Z';
+    } else if ((initials.length < 2) ||
+        (initials.length < 2 && fullName.length < 3)) {
+      return 'The Name Must Be Complete';
+    }
+    return null;
+  }
 }
--- test/fluttely_form_validations_test.dart
+++ test/fluttely_form_validations_test.dart
@@ -1,7 +1,41 @@
 import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:fluttely_form_validations/fluttely_form_validations.dart';
+import 'package:fluttely_form_validations/fluttely_form_validations.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('1', () {
+    final String? fullName = FormValidations.fullName(null);
+
+    expect(fullName, 'this field can not be empty');
+  });
+  test('1.1', () {
+    final String? fullName = FormValidations.fullName('');
+
+    expect(fullName, 'this field can not be empty');
+  });
+  test('3', () {
+    final String? fullName = FormValidations.fullName('Kevin');
+
+    expect(fullName, 'The Name Must Be Complete');
+  });
+  test('4', () {
+    final String? fullName = FormValidations.fullName('Kevin Kobori 1');
+
+    expect(fullName, 'Enter Characters From A To Z');
+  });
+  test('4.1', () {
+    final String? fullName = FormValidations.fullName('Kevin Kobori ?');
+
+    expect(fullName, 'Enter Characters From A To Z');
+  });
+  test('5', () {
+    final String? fullName = FormValidations.fullName('Kevin');
+
+    expect(fullName, 'The Name Must Be Complete');
+  });
+  test('6', () {
+    final String? fullName = FormValidations.fullName('Kevin Kobori');
+
+    expect(fullName, null);
+  });
 }

4572856cd5e6edd1f1c69afd7574c6d98a1e23c0
KevinKobori <kevin.kobori@hotmail.com>
2023-09-29 19:25:45 -0300

    first commit

---
+++ lib/fluttely_form_validations.dart
@@ -0,0 +1,7 @@
+library fluttely_form_validations;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/fluttely_form_validations_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// import 'package:fluttely_form_validations/fluttely_form_validations.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
